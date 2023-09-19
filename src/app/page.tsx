fc3c94e02158acfeae4aa7ccc81c981472a9c841
Luca <luca.kanto@gmail.com>
2023-09-19 14:32:16 +0200

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Hello</h1>
+      <h1>Hello world</h1>
     </main>
   )
 }
